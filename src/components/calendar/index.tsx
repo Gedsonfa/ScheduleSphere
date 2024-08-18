98099dab1eac62222f43623ac7624605d954c2ea
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2024-08-18 17:42:10 -0300

    calculando os dias do mês

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -8,31 +8,47 @@ import {
   CalendarTittle,
 } from './styles'
 import { getWeekDays } from '../../utils/get-week-days'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
-
   function handlePreviousMonth() {
     const previousMonthDate = currentDate.subtract(1, 'month')
-
     setCurrentDate(previousMonthDate)
   }
-
   function handleNextsMonth() {
     const nextMonthDate = currentDate.add(1, 'month')
-
     setCurrentDate(nextMonthDate)
   }
-
   const shortWeekDays = getWeekDays({ short: true })
 
   const currentMonth = currentDate.format('MMMM')
-
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
@@ -40,7 +56,6 @@ export function Calendar() {
         <CalendarTittle>
           {currentMonth} <span>{currentYear}</span>
         </CalendarTittle>
-
         <CalendarActions>
           <button onClick={handlePreviousMonth} title="Mês anterior">
             <CaretLeft />
